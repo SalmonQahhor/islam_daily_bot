e6d27aaf5b5126ec1d91811f53b434b97327c101
Salmon <salmonbekqahhorov@gmail.com>
2026-01-15 19:52:12 +0500

    Update ayat.py

--- ayat.py
+++ ayat.py
@@ -1,13 +1,25 @@
 import requests
 import random
+from trans import to_latin  
 
-def get_random_ayat():
+def get_random_ayat(lang="latin"):
     try:
         ayat_number = random.randint(1, 6236)
         url = f"https://api.alquran.cloud/v1/ayah/{ayat_number}/uz.sodik"
         res = requests.get(url).json()
         data = res["data"]
-        return f"📖 {data['text']}\n\n({data['surah']['englishName']}, {data['numberInSurah']})"
+        
+        text = data['text']
+        surah_name = data['surah']['englishName']
+        ayah_num = data['numberInSurah']
+        
+       
+        if lang == "latin":
+            text = to_latin(text)
+            final_msg = f"📖 {text}\n\n({surah_name}, {ayah_num})"
+        else:
+            final_msg = f"📖 {text}\n\n({surah_name}, {ayah_num})"
+            
+        return final_msg
     except:
-        return "⚠️ Hozircha oyat yuklashda xatolik yuz berdi. Birozdan so'ng urinib ko'ring."
-
+        return "⚠️ Xatolik yuz berdi."
