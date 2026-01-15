d8f56574eea4b3305a1ce65101dac02202f1f773
Salmon <salmonbekqahhorov@gmail.com>
2026-01-15 19:52:48 +0500

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,26 @@
+TEXTS = {
+    "latin": {
+        "start": "Assalomu alaykum! Namoz vaqtlari botiga xush kelibsiz.\nIltimos, viloyatingizni tanlang:",
+        "menu_prayer": "📅 Bugungi namoz vaqtlari",
+        "menu_ayat": "📖 Tasodifiy oyat",
+        "menu_hadis": "📜 Tasodifiy hadis",
+        "menu_region": "📍 Viloyatni o'zgartirish",
+        "menu_lang": "🔤 Кирилл alifbosiga",
+        "menu_stat": "📊 Statistika(admin)",
+        "source": "\n🌐 Manba: aladhan.com API",
+        "select_region": "Iltimos, yashash viloyatingizni tanlang:",
+        "region_saved": "✅ Viloyat saqlandi: "
+    },
+    "cyrillic": {
+        "start": "Ассалому алайкум! Намоз вақтлари ботига хуш келибсиз.\nИлтимос, вилоятингизни танланг:",
+        "menu_prayer": "📅 Бугунги намоз вақтлари",
+        "menu_ayat": "📖 Тасодифий оят",
+        "menu_hadis": "📜 Тасодифий ҳадис",
+        "menu_region": "📍 Вилоятни ўзгартириш",
+        "menu_lang": "🔤 Lotin alifbosiga",
+        "menu_stat": "📊 Статистика(admin)",
+        "source": "\n🌐 Манба: aladhan.com API",
+        "select_region": "Илтимос, яшаш вилоятингизни танланг:",
+        "region_saved": "✅ Вилоят сақланди: "
+    }
+}
