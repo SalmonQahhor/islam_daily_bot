f807a1f958b34babe04d3215a57ee54ea180e98e
Salmon <salmonbekqahhorov@gmail.com>
2026-01-15 10:42:45 +0500

    Implement error handling in get_random_ayat function
    Added error handling for API request in get_random_ayat function.

--- ayat.py
+++ ayat.py
@@ -2,9 +2,12 @@ import requests
 import random
 
 def get_random_ayat():
-    ayat_number = random.randint(1, 6236)
-    url = f"https://api.alquran.cloud/v1/ayah/{ayat_number}/uz.sodik"
-    res = requests.get(url).json()
-    data = res["data"]
+    try:
+        ayat_number = random.randint(1, 6236)
+        url = f"https://api.alquran.cloud/v1/ayah/{ayat_number}/uz.sodik"
+        res = requests.get(url).json()
+        data = res["data"]
+        return f"📖 {data['text']}\n\n({data['surah']['englishName']}, {data['numberInSurah']})"
+    except:
+        return "⚠️ Hozircha oyat yuklashda xatolik yuz berdi. Birozdan so'ng urinib ko'ring."
 
-    return f"📖 {data['text']}\n\n({data['surah']['englishName']}, {data['numberInSurah']})"
